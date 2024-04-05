144b3766c6c61c14b7a37db97cae66965fafc489
Benjamin Moro <benjaminnmoro@gmail.com>
2024-04-05 15:17:59 -0300

    Cosas Clase 2

---
+++ ClasesLabo/Clase2/src/main/java/org/example/App.java
@@ -0,0 +1,18 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+        String cadena = "hOla como andas";
+        String string2 = cadena.trim();
+
+        System.out.println(string2);
+        System.out.println(cadena.toUpperCase());
+    }
+}
